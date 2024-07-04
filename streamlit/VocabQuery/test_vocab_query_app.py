6628c8b6a1a012f36d36912321434f0c2918cd29
Philo-W <philo_wu@outlook.com>
2024-07-04 15:29:41 +1000

    Update test_vocab_query_app.py
    Updated the regex pattern of en_meaning; fixed the bug that when a list
    is exhausted and the user switch to a new list, the data failed to
    update properly (start from first)

--- streamlit/VocabQuery/test_vocab_query_app.py
+++ streamlit/VocabQuery/test_vocab_query_app.py
@@ -1,6 +1,9 @@
+import re
+
 from pandas import DataFrame
 
 import streamlit as st
+from streamlit import session_state as session
 
 
 def start_revise(current_vocab: DataFrame):
@@ -16,9 +19,16 @@ def start_revise(current_vocab: DataFrame):
 
 def show_definition(current_vocab: DataFrame):
     ch_meaning = current_vocab["ch_meaning"].split("；")  # Chinese semicolon
-    en_meaning = current_vocab["en_meaning"].split("; ")  # English semicolon
+    en_meaning_full = current_vocab["en_meaning"]
+    if "1" not in en_meaning_full:
+        en_meaning = [en_meaning_full]
+    else:
+        pattern = r"\d+\.\s*([^;]+)(?:;|\s|$)"
+        en_meaning = re.findall(pattern, en_meaning_full)
+
     st.markdown("**Definition:**")
     for ch, en in zip(ch_meaning, en_meaning):
+
         meaning = f"""
                 {ch.strip()}\\
                 {en.split('.')[-1]}
@@ -29,74 +39,58 @@ def show_definition(current_vocab: DataFrame):
 # ==============================
 # DEFINE SESSION STATES
 # ==============================
-if "cur_q_idx" not in st.session_state:
-    st.session_state.cur_q_idx = 0
-if "init_sample" not in st.session_state:
-    st.session_state.init_sample = True
-if "start" not in st.session_state:
-    st.session_state.start = True
-if "vocab_list" not in st.session_state:
-    st.session_state.vocab_list = None
-if "list_data" not in st.session_state:
-    st.session_state.list_data = None
-if "current_vocab" not in st.session_state:
-    st.session_state.current_vocab = None
+if "cur_q_idx" not in session:
+    session.cur_q_idx = 0
+if "init_sample" not in session:
+    session.init_sample = True
+if "start" not in session:
+    session.start = True
+if "prev_vocab_list" not in session:
+    session.prev_vocab_list = "list1"
+if "vocab_list" not in session:
+    session.vocab_list = None
+if "list_data" not in session:
+    session.list_data = None
+if "current_vocab" not in session:
+    session.current_vocab = None
 # ==============================
 # MAIN APP EXECUTION STARTS HERE
 # ==============================
 st.sidebar.markdown("# Select Vocabulary list:")
-st.session_state.vocab_list = st.sidebar.selectbox(
-    "", [f"list{i}" for i in range(1, 33)]
-)
+session.vocab_list = st.sidebar.selectbox("", [f"list{i}" for i in range(1, 33)])
 
 conn = st.connection("gre_vocabulary_db", type="sql")
 data = conn.query("SELECT * FROM vocabulary.gre_3000 WHERE equation_1 IS NOT NULL")
-if "data" not in st.session_state:
-    st.session_state.data = data
+if "data" not in session:
+    session.data = data
 
-st.session_state.list_data = st.session_state.data[
-    st.session_state.data["list"] == st.session_state.vocab_list
-]
+if session.vocab_list is not None:
+    session.list_data = session.data[session.data["list"] == session.vocab_list]
 
-if st.session_state.init_sample:
-    st.session_state.data = st.session_state.data.sample(frac=1).reset_index(drop=True)
-    st.session_state.init_sample = False
+    if session.vocab_list != session.prev_vocab_list:
+        session.prev_vocab_list = session.vocab_list
+        session.cur_q_idx = 0
+
+if session.init_sample:
+    session.data = session.data.sample(frac=1).reset_index(drop=True)
+    session.init_sample = False
 else:
     pass
 
-
-# if st.session_state.start:
-#    st.session_state.start = False
-#    st.session_state.current_vocab = st.session_state.list_data.iloc[st.session_state.cur_q_idx, :]
-#    start_revise(st.session_state.current_vocab)
-#    click_show_definition = st.button("show definition", type="secondary")
-#    if click_show_definition:
-#        show_definition(st.session_state.current_vocab)
-
 left, right = st.columns(2)
 if left.button("Previous", key="Previous", type="primary", use_container_width=True):
-    st.session_state.cur_q_idx -= 1
-#    st.session_state.current_vocab = st.session_state.list_data.iloc[
-#        st.session_state.cur_q_idx, :
-#    ]
-#    start_revise(st.session_state.current_vocab)
-#    click_show_definition = st.button("show definition", type="secondary")
-#    if click_show_definition:
-#        show_definition(st.session_state.current_vocab)
-if right.button("Next", key="Next", type="primary", use_container_width=True):
-    st.session_state.cur_q_idx += 1
-    #    st.session_state.current_vocab = st.session_state.list_data.iloc[
-    #        st.session_state.cur_q_idx, :
-    #    ]
-    #    start_revise(st.session_state.current_vocab)
-    #    click_show_definition = st.button("show definition", type="secondary")
-    #    if click_show_definition:
-    #        show_definition(st.session_state.current_vocab)
+    session.cur_q_idx -= 1
 
-st.session_state.current_vocab = st.session_state.list_data.iloc[
-    st.session_state.cur_q_idx, :
-]
-start_revise(st.session_state.current_vocab)
+if right.button("Next", key="Next", type="primary", use_container_width=True):
+    session.cur_q_idx += 1
+
+session.current_vocab = session.list_data.iloc[session.cur_q_idx, :]
+st.write(session.cur_q_idx)
+start_revise(session.current_vocab)
 click_show_definition = st.button("show definition", type="secondary")
-if click_show_definition:
-    show_definition(st.session_state.current_vocab)
+
+if True:
+    show_definition(session.current_vocab)
+# TODO: delete above
+# if click_show_definition:
+#     show_definition(session.current_vocab)
