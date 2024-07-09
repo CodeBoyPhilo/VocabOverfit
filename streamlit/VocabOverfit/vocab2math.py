a2fec03afc62dd04f253dbe334d4dc85a79475ee
Philo-W <philo_wu@outlook.com>
2024-07-10 00:05:47 +1000

    Added a query feature

--- streamlit/VocabOverfit/vocab2math.py
+++ streamlit/VocabOverfit/vocab2math.py
@@ -8,9 +8,60 @@ import streamlit as st
 from streamlit import session_state as session
 
 
-def query(vocab):
-    start_revise(vocab)
-    show_definition(vocab)
+def show_greeting_message():
+
+    html_content = """
+    # <a href="#"><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25px" height="25px"></a> Welcome to Vocab2Math!
+    """
+    acknowledgement = """
+    ## 	:bulb: Acknowledgement
+    The vocabulary details are adapted from [**liurui39660/3000**](https://github.com/liurui39660/3000) and **张巍老师GRE**.
+    """
+
+    how_to_use = """
+    ## 	:package: How to use?
+    1. Select a vocabulary list to study!
+    2. Finished? Select a new list! 
+    """
+
+    st.sidebar.markdown(html_content, unsafe_allow_html=True)
+    st.sidebar.markdown("")
+    st.sidebar.markdown("by: [CodeBoyPhilo](https://github.com/CodeBoyPhilo)")
+    st.sidebar.divider()
+    st.sidebar.markdown(acknowledgement)
+    st.sidebar.markdown(how_to_use)
+
+
+def query(query_vocab):
+    if query_vocab != "":
+        if query_vocab in session.data["vocabulary"].values:
+            vocab = session.data[session.data["vocabulary"] == query_vocab]
+            vocabulary = vocab["vocabulary"].values[0]
+            equation_1 = vocab["equation_1"].values[0].split("=")[-1]
+            equation_2 = vocab["equation_2"].values[0].split("=")[-1]
+
+            st.sidebar.markdown(f"## {vocabulary}")
+            st.sidebar.markdown(f"##### = {equation_1}")
+            st.sidebar.markdown(f"##### = {equation_2}")
+
+            ch_meaning = vocab["ch_meaning"].values[0].split("；")  # Chinese semicolon
+            en_meaning_full = vocab["en_meaning"].values[0]
+            if "1" not in en_meaning_full:
+                en_meaning = [en_meaning_full]
+            else:
+                pattern = r"\d+\.\s*([^;]+)(?:;|\s|$)"
+                en_meaning = re.findall(pattern, en_meaning_full)
+
+            # st.markdown("**Definition:**")
+            for ch, en in zip(ch_meaning, en_meaning):
+
+                meaning = f"""
+                        {ch.strip()}\\
+                        {en.split('.')[-1]}
+                """
+                st.sidebar.markdown(meaning)
+        else:
+            st.sidebar.warning("Vocab not found!", icon="⚠️")
 
 
 def start_revise(current_vocab: DataFrame):
@@ -71,6 +122,8 @@ if "current_vocab" not in session:
 # ==============================
 
 # -------Sidebar Element-------
+show_greeting_message()
+st.sidebar.divider()
 session.vocab_list = st.sidebar.selectbox(
     "**Select a vocabulary list**",
     [f"list{i}" for i in range(1, 33)],
@@ -88,9 +141,13 @@ session.always_show_def = st.sidebar.radio(
 data = pd.read_csv(DATA_DIR)
 if "data" not in session:
     session.data = data
-if st.sidebar.button("Shuffle the order", type="primary"):
+
+if st.sidebar.button("Shuffle the order", type="primary", use_container_width=True):
     session.data = session.data.sample(frac=1).reset_index(drop=True)
     session.cur_v_idx = 0
+
+query_vocab = st.sidebar.text_input("Query a vocab")
+query(query_vocab)
 
 if session.vocab_list is not None:
     session.list_data = session.data[session.data["list"] == session.vocab_list]
